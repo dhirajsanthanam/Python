fabce11d74a45cc7af6258641f71100f4656dd46
Dhiraj Santhanam <dhirajsanthanam@gmail.com>
2025-05-06 21:17:50 +1000

    Temperature Calculation

---
+++ Testing waters and learning Python/what is the temp.py
@@ -0,0 +1,9 @@
+x = float(input("what is the temp in celcius: "))
+f=((x*9/5)+32)
+print("temerature in Fahrenheit is :",f)
+
+f=((0*(9/5)+32))
+print("temerature in Fahrenheit is :",f)
+
+f=((100*(9/5)+32))
+print("temerature in Fahrenheit is :",f)
